887b2f7ced963ca75a0aea10fdcbe9d53bec120c
JamesVanWaza <jgacuca@gmail.com>
2021-02-17 12:37:08 -0500

    WIP, still trying to deploy to see if its going to work

--- index.js
+++ index.js
@@ -3,18 +3,18 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 // proxy middleware options
 const filter = function(pathname, req) {
-    return true;
+    return ((req.headers.origin === 'http://127.0.0.1:5500') ||
+          (req.headers.origin === 'https://127.0.0.1:5500'));
 };
 
 // proxy middleware options
 const options = {
     target: 'https://developer.nps.gov', // target host
     changeOrigin: true, // needed for virtual hosted sites
-    ws: true, // proxy websockets
     pathRewrite: {
         '^/nps/': '/', // rewrite path
     },
-    onProxyReq: (proxyReq) => {
+    onProxyReq: (proxyReq, req, res) => {
         // append key-value pair for API key to end of path
         // using KEYNAME provided by web service
         // and KEYVALUE stored in Heroku environment variable
@@ -24,7 +24,7 @@ const options = {
 };
 
 // create the proxy (without context)
-const exampleProxy = createProxyMiddleware(options);
+const exampleProxy = createProxyMiddleware(filter, options);
 
 // mount `exampleProxy` in web server
 const app = express();
